2f7023eb4cc9fb1e7bcb8001adb08c768fc19797
Leo Ho <108187231+leoho55688@users.noreply.github.com>
2022-08-05 13:14:21 +0800

    fix bugs and can start from a specific post

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,17 @@
 const { Builder, By, until, Key } = require("selenium-webdriver");
 const fs = require("fs-extra");
 const chrome = require("selenium-webdriver/chrome");
+const should = require("chai").should();
 require("chromedriver");
 
-async function postLike(postID, driver, end_cursor_like = null) {
+async function postLike(
+  id,
+  postID,
+  userName,
+  password,
+  driver,
+  end_cursor_like = null
+) {
   // No. of times needed to get all user data
   let count;
   // To record if there is next page
@@ -12,9 +20,21 @@ async function postLike(postID, driver, end_cursor_like = null) {
   function random(max) {
     return Math.random() * max;
   }
+  // enter text word by word
+  async function sendHumanKeys(element, keys) {
+    for (let i = 0; i < keys.length; i++) {
+      await element.sendKeys(keys[i]);
+      try {
+        await driver.sleep(random(500));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
   fs.appendFile(
     `./posts/${postID}.txt`,
-    "Username of people who liked the post:\n",
+    `Post No.:${id}\nUsername of people who liked the post:\n`,
     (err) => {
       if (err) {
         console.log(err);
@@ -23,6 +43,9 @@ async function postLike(postID, driver, end_cursor_like = null) {
   );
   console.log(end_cursor_like);
 
+  await driver.getAllWindowHandles().then(async function (handles) {
+    return driver.switchTo().window(handles[1]);
+  });
   // if no end cursor, get first 50 users
   if (end_cursor_like == null) {
     await driver.get(
@@ -39,11 +62,15 @@ async function postLike(postID, driver, end_cursor_like = null) {
         count =
           Math.floor(likes.data.shortcode_media.edge_liked_by.count / 50) + 1;
         liker.forEach((fol) => {
-          fs.appendFile(`./${postID}.txt`, `${fol.node.username}\n`, (err) => {
-            if (err) {
-              console.log(err);
+          fs.appendFile(
+            `./posts/${postID}.txt`,
+            `${fol.node.username}\n`,
+            (err) => {
+              if (err) {
+                console.log(err);
+              }
             }
-          });
+          );
         });
       });
     await driver.sleep(random(5000));
@@ -71,7 +98,7 @@ async function postLike(postID, driver, end_cursor_like = null) {
             likes.data.shortcode_media.edge_liked_by.page_info.has_next_page;
           liker.forEach((fol) => {
             fs.appendFile(
-              `./${postID}.txt`,
+              `./posts/${postID}.txt`,
               `${fol.node.username}\n`,
               (err) => {
                 if (err) {
@@ -95,6 +122,7 @@ async function postLike(postID, driver, end_cursor_like = null) {
       return driver.switchTo().window(handles[0]);
     });
     await driver.sleep(random(2500));
+    await driver.wait(until.elementsLocated(By.css("div._acut"), 10000));
     let el = await driver.findElements(By.css("div._acut"));
     let temp = Math.floor(random(5));
     await el[temp].click();
@@ -139,14 +167,32 @@ async function postLike(postID, driver, end_cursor_like = null) {
   await driver.wait(until.elementsLocated(By.css("div._abm4"), 10000));
   let button = await driver.findElements(By.css("div._abm4"));
   await button[1].click();
-  await driver.quit();
+  await driver.sleep(random(50000));
+  // login ig account
+  let account = await driver.findElement(By.name("username"));
+  await sendHumanKeys(account, userName);
+  await driver.sleep(random(5000));
+  let accountPass = await driver.findElement(By.name("password"));
+  await sendHumanKeys(accountPass, password);
+  await driver.findElement(By.css("button.sqdOP.L3NKy.y3zKF")).click();
+  await driver.sleep(15000);
+  await driver.findElement(By.css("button.sqdOP.yWX7d.y3zKF")).click();
+  await driver.wait(until.elementsLocated(By.css("button._a9--._a9_1"), 10000));
+  await driver.findElement(By.css("button._a9--._a9_1")).click();
+  await driver.sleep(random(5000));
 }
 
 // the main function
-async function getLike(target, userName, password, end_cursor_like = null) {
+async function getLike(
+  target,
+  userName,
+  password,
+  shortcode = null,
+  end_cursor_like = null
+) {
   let postID = [];
   let postNo;
-  // get random no. from 0-5000
+  // get random no. with maximum max
   function random(max) {
     return Math.random() * max;
   }
@@ -198,28 +244,10 @@ async function getLike(target, userName, password, end_cursor_like = null) {
   // start querying shortcode of kol's posts
   // get first 50 shortcodes
   await driver.switchTo().newWindow("tab");
-  await driver.get(
-    `https://www.instagram.com/graphql/query/?query_hash=69cba40317214236af40e7efa697781d&variables={%22id%22:%22${target}%22,%22first%22:50}`
-  );
-  await driver
-    .findElement(By.xpath("/html/body/pre"))
-    .getText()
-    .then((text) => {
-      let posts = JSON.parse(text);
-      let post = posts.data.user.edge_owner_to_timeline_media.edges;
-      end_cursor =
-        posts.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
-      postNo = posts.data.user.edge_owner_to_timeline_media.count;
-      for (let i = 0; i < 50; i++) {
-        postID[i] = post[i].node.shortcode;
-      }
-    });
 
-  // get the rest shortcodes
-  let count = Math.floor(postNo / 50);
-  for (let i = 1; i <= count; i++) {
+  if (shortcode == null) {
     await driver.get(
-      `https://www.instagram.com/graphql/query/?query_hash=69cba40317214236af40e7efa697781d&variables={%22id%22:%22${target}%22,%22first%22:50,%22after%22:%22${end_cursor}%22}`
+      `https://www.instagram.com/graphql/query/?query_hash=69cba40317214236af40e7efa697781d&variables={%22id%22:%22${target}%22,%22first%22:50}`
     );
     await driver
       .findElement(By.xpath("/html/body/pre"))
@@ -229,36 +257,71 @@ async function getLike(target, userName, password, end_cursor_like = null) {
         let post = posts.data.user.edge_owner_to_timeline_media.edges;
         end_cursor =
           posts.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
-        for (let j = 0; j < 50; j++) {
-          if (post[j] == null) {
-            break;
-          }
-          postID[50 * i + j] = post[j].node.shortcode;
+        postNo = posts.data.user.edge_owner_to_timeline_media.count;
+        for (let i = 0; i < 50; i++) {
+          postID[i] = post[i].node.shortcode;
         }
       });
-    await driver.sleep(random(15000));
-  }
 
-  // start querying the user who liked the post
-  console.log(postID);
-  for (let i = 0; i < postNo; i++) {
-    fs.appendFileSync("./post.txt", `${postID[i]}\n`, (err) => {
+    // get the rest shortcodes
+    let count = Math.floor(postNo / 50);
+    for (let i = 1; i <= count; i++) {
+      await driver.get(
+        `https://www.instagram.com/graphql/query/?query_hash=69cba40317214236af40e7efa697781d&variables={%22id%22:%22${target}%22,%22first%22:50,%22after%22:%22${end_cursor}%22}`
+      );
+      await driver
+        .findElement(By.xpath("/html/body/pre"))
+        .getText()
+        .then((text) => {
+          let posts = JSON.parse(text);
+          let post = posts.data.user.edge_owner_to_timeline_media.edges;
+          end_cursor =
+            posts.data.user.edge_owner_to_timeline_media.page_info.end_cursor;
+          for (let j = 0; j < 50; j++) {
+            if (post[j] == null) {
+              break;
+            }
+            postID[50 * i + j] = post[j].node.shortcode;
+          }
+        });
+      await driver.sleep(random(15000));
+    }
+
+    // start querying the user who liked the post
+    // record the shortcodes of posts
+    console.log(postID);
+    let shortcodes = postID.toString();
+
+    fs.appendFileSync("./post.txt", shortcodes, (err) => {
       if (err) {
         console.log(err);
       }
     });
+
+    // get usernames of every posts
+    for (let i = 0; i < postNo; i++) {
+      await postLike(postID[i], userName, password, driver);
+    }
+  } else {
+    let a;
+    fs.readFileSync("post.txt", "utf8", function (err, data) {
+      postID = data.split(",");
+    });
+    for (let i = 0; i < postID.length; i++) {
+      try {
+        postID[i].should.equal(shortcode);
+      } catch (e) {
+        continue;
+      }
+      a = i;
+      break;
+    }
+    for (let i = a; i < postNo; i++) {
+      await postLike(i, postID[i], userName, password, driver);
+    }
   }
-  // postID.forEach((post) => {
-  //   fs.appendFile("./post.txt", `${post}\n`, (err) => {
-  //     if (err) {
-  //       console.log(err);
-  //     }
-  //   });
-  // });
-  postID.asyncforEach((shortcode) => {
-    await postLike(shortcode, driver);
-    await driver.sleep(random()*100);
-  });
+
+  await driver.quit();
 }
 
 getLike(273583714, "ceplutenocnarovich", "look4kol");
